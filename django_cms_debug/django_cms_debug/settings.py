0c01e6b80c42c9dfa30623e50372d1a210b4ca0a
Thomas Remmert <thomas.remmert@gmail.com>
2012-06-27 02:53:40 +0100

    adding contrib admin and the right cms urls

--- django_cms_debug/django_cms_debug/settings.py
+++ django_cms_debug/django_cms_debug/settings.py
@@ -146,7 +146,7 @@ INSTALLED_APPS = (
     'south',
     'sekizai',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'cms.plugins.flash',
